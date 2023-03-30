7e980cfed448c8735b55e9fd6878971bdd331901
Xackery Xtal <xackery@gmail.com>
2023-03-30 07:10:26 -0700

    Add support for macOS

--- addons/aseprite_importer/Context.gd
+++ addons/aseprite_importer/Context.gd
@@ -6,11 +6,11 @@ var editor_settings:EditorSettings
 
 func get_global_filepath(filepath) -> String:
 	return ProjectSettings.globalize_path(filepath)
-	
+
 func get_data_path(filepath) -> String:
 	var name:String = filepath.get_file()
 	return OS.get_cache_dir().path_join("%s/%s-data.json" % [ProjectSettings.get_setting("application/config/name"), name])
-		
+
 func get_sheet_path(filepath) -> String:
 	var name:String = filepath.get_file()
 	return OS.get_cache_dir().path_join("%s/%s-sheet.png" % [ProjectSettings.get_setting("application/config/name"), name])
@@ -18,7 +18,7 @@ func get_sheet_path(filepath) -> String:
 func get_user_data_path(filepath) -> String:
 	var name:String = filepath.get_file()
 	return OS.get_cache_dir().path_join("%s/%s-user-data.json" % [ProjectSettings.get_setting("application/config/name"), name])
-	
+
 func get_tile_set_data_path(filepath) -> String:
 	var name:String = filepath.get_file()
 	return OS.get_cache_dir().path_join("%s/%s-tile-set-data.json" % [ProjectSettings.get_setting("application/config/name"), name])
@@ -36,7 +36,7 @@ func find_aseprite() -> String:
 		var aseprite_path = editor_settings.get("filesystem/import/aseprite/path")
 		if test_aseprite_path(aseprite_path):
 			return aseprite_path
-	
+
 	var locations:PackedStringArray = []
 	match OS.get_name():
 		"Windows":
@@ -45,15 +45,21 @@ func find_aseprite() -> String:
 			for ftype in output:
 				var exe_path = ftype.split("=", false, 1)[1]
 				locations.append(exe_path)
-			
+
 			locations.append("C:/Program Files (x86)/Steam/steamapps/common/Aseprite/Aseprite.exe")
 			locations.append("C:/Program Files (x86)/Aseprite/Aseprite.exe")
 			locations.append("C:/Program Files/Aseprite/Aseprite.exe")
-			
+
 		"iOS":
 			locations.append("~/Library/ApplicationSupport/Steam/steamapps/common/Aseprite/Aseprite.app/Contents/MacOS/aseprite")
+			locations.append("~/Library/Application Support/Steam/SteamApps/common/Aseprite/Aseprite.app/Contents/MacOS/aseprite")
 			locations.append("/Applications/Aseprite.app/Contents/MacOS/aseprite")
-	
+
+		"macOS":
+			locations.append("~/Library/ApplicationSupport/Steam/steamapps/common/Aseprite/Aseprite.app/Contents/MacOS/aseprite")
+			locations.append("~/Library/Application Support/Steam/SteamApps/common/Aseprite/Aseprite.app/Contents/MacOS/aseprite")
+			locations.append("/Applications/Aseprite.app/Contents/MacOS/aseprite")
+
 	for location in locations:
 		if FileAccess.file_exists(location) and test_aseprite_path(location):
 			editor_settings.set("filesystem/import/aseprite/path", location)
@@ -63,7 +69,7 @@ func find_aseprite() -> String:
 				hint = PROPERTY_HINT_GLOBAL_FILE
 			})
 			return location
-	
+
 	push_warning("Aseprite not found. Please set the path to Aseprite in the Editor Settings. (Project -> Project Settings -> Filesystem -> Import -> Aseprite -> Path)")
 	editor_settings.set("filesystem/import/aseprite/path", "")
 	editor_settings.add_property_info({
